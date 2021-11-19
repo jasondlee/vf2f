95cc195113b4540f053aed68e8c77f3bfa462fd5
Jason Lee <jason@steeplesoft.com>
2021-11-19 09:20:29 -0600

    Set up basic MDB

---
+++ src/main/java/com/redhat/eap/vf2f/MessageBean.java
@@ -0,0 +1,31 @@
+package com.redhat.eap.vf2f;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(name = "GreetingQueueMDB", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/GreetingQueue"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
+public class MessageBean implements MessageListener {
+
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage msg = null;
+        try {
+            if (message instanceof TextMessage) {
+                msg = (TextMessage) message;
+                System.out.println("Received Message from queue: " + msg.getText());
+            } else {
+                System.out.println("Message of wrong type: " + message.getClass().getName());
+            }
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
